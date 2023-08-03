5c09439866258dd4eae3a15bda68d2b882b18e8e
fishenchips <philipcarlsson@hotmail.com>
2023-08-03 13:26:42 +0200

    creating reactive test form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,7 @@ import { RecipesComponent } from './recipes/recipes.component';
     HomeComponent,
     RecipesComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  testForm: FormGroup;
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  onSubmit() {
+    console.log(this.testForm);
+    console.log('hej');
+  }
+
+  private initForm() {
+    let description = '';
+    let no = '';
+
+    this.testForm = new FormGroup({
+      text: new FormControl(description),
+      number: new FormControl(no),
+    });
+  }
+}
